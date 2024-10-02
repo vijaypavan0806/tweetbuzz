df689f79772a0b43f22b1145851a3e67bfe4059b
vijaypavan0806 <adimulam.vijaypavan2022@vitstudent.ac.in>
2024-10-02 09:47:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const TwitterLite = require('twitter-lite');
 
 const app = express();
-require('dotenv').config();
 
-
+const client = new TwitterLite({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+});
 
 app.get("/auth/twitter", async (req, res) => {
   try {
-    const response = await client.getRequestToken("YOUR_CALLBACK_URL");
+    const response = await client.getRequestToken(process.env.CALLBACK_URL);
 
     if (response.oauth_callback_confirmed) {
       res.redirect(`https://api.twitter.com/oauth/authenticate?oauth_token=${response.oauth_token}`);
@@ -28,7 +31,12 @@ app.get("/auth/twitter/callback", async (req, res) => {
       oauth_token,
     });
 
-    
+    const userClient = new TwitterLite({
+      consumer_key: process.env.CONSUMER_KEY,
+      consumer_secret: process.env.CONSUMER_SECRET,
+      access_token_key: response.oauth_token,
+      access_token_secret: response.oauth_token_secret,
+    });
 
     const user = await userClient.get("account/verify_credentials");
     res.send(`Welcome, ${user.name}!`);
